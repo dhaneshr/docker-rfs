7b901c6313bb92dac1aa5fa07b78f33b3e79a549
Dhanesh Ramachandram <dhanesh.ramachandram@vectorinstitute.ai>
2024-11-05 11:19:16 -0500

    first version with larp model

--- app.py
+++ app.py
@@ -20,45 +20,42 @@ from enum import Enum
 
 DATABASE_PATH = '/app/local_data/predictions.db'
 
-
 # Activate automatic conversion of pandas DataFrames to R DataFrames
 pandas2ri.activate()
 
-# Configure logging
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Load R libraries and model
+
 def load_r_libraries():
     robjects.r('library(randomForestSRC)')
     robjects.r('library(ggplot2)')
     robjects.r('library(scales)')
     robjects.r('library(survival)')
 
-
 def load_r_model():
     try:
-        robjects.r('load("follic_model.RData")')
-        if 'follic.obj' in robjects.globalenv:
+        robjects.r('load("dummy_model_clean2.RData")')
+        if 'model' in robjects.globalenv:
             logger.info("R model loaded successfully.")
         else:
-            logger.error("R model object 'follic.obj' not found in global environment.")
+            logger.error("R model object 'model' not found in global environment.")
             raise ValueError("R model object not found.")
     except Exception as e:
         logger.error("Error loading R model: %s", e)
         raise e
 
-
 load_r_libraries()
 load_r_model()
 
-# Initialize FastAPI
+
 app = FastAPI()
 
 # Serve static files from the "resources" directory
 app.mount("/resources", StaticFiles(directory="resources"), name="resources")
 
-# Set up Jinja2 templates for HTML files
+
 templates = Jinja2Templates(directory="templates")
 
 # Enable CORS to allow requests from the web page
@@ -70,7 +67,7 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-# Database dependency
+
 def get_db():
     if not os.path.exists(DATABASE_PATH):
         logger.warning("Database not found. Initializing...")
@@ -81,7 +78,6 @@ def get_db():
         yield conn
     finally:
         conn.close()
-
 
 
 def initialize_db():
@@ -98,13 +94,25 @@ def initialize_db():
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS predictions (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                patientId TEXT,
-                hospitalId TEXT,
                 age INTEGER,
-                hgb REAL,
-                clinstg INTEGER,
-                ch TEXT,
-                rt TEXT,
+                sex_f INTEGER,
+                elective_adm INTEGER,
+                homelessness INTEGER,
+                peripheral_AD INTEGER,
+                coronary_AD INTEGER,
+                stroke INTEGER,
+                CHF INTEGER,
+                hypertension INTEGER,
+                COPD INTEGER,
+                CKD INTEGER,
+                malignancy INTEGER,
+                mental_illness INTEGER,
+                creatinine REAL,
+                Hb_A1C REAL,
+                albumin REAL,
+                Hb_A1C_missing INTEGER,
+                creatinine_missing INTEGER,
+                albumin_missing INTEGER,
                 predict_at INTEGER,
                 prediction TEXT,
                 timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
@@ -131,32 +139,50 @@ def initialize_db():
 # Initialize the database once during startup
 initialize_db()
 
-class YesNo(str, Enum):
-    Y = "Y"
-    N = "N"
-
 class ModelInput(BaseModel):
-    patientId: Optional[str] = None
-    hospitalId: Optional[str] = None
-    age: float
-    hgb: float
-    clinstg: int
-    ch: YesNo
-    rt: YesNo
+    age: int
+    sex_f: int
+    elective_adm: int
+    homelessness: int
+    peripheral_AD: int
+    coronary_AD: int
+    stroke: int
+    CHF: int
+    hypertension: int
+    COPD: int
+    CKD: int
+    malignancy: int
+    mental_illness: int
+    creatinine: Optional[float] = None
+    Hb_A1C: Optional[float] = None
+    albumin: Optional[float] = None
+    Hb_A1C_missing: int
+    creatinine_missing: int
+    albumin_missing: int
     predict_at: int
-
-    class Config:
-        use_enum_values = True  # Ensures enums are converted to their values
 
 # Helper functions
 def prepare_input_data(input: ModelInput) -> pd.DataFrame:
     data = {
         'age': [input.age],
-        'hgb': [input.hgb],
-        'clinstg': [input.clinstg],
-        'ch': [str(input.ch)],     # Explicitly convert to string
-        'rt': [str(input.rt)],     # Explicitly convert to string
-        'predict_at': [input.predict_at]
+        'sex_f': [input.sex_f],
+        'elective_adm': [input.elective_adm],
+        'homelessness': [input.homelessness],
+        'peripheral_AD': [input.peripheral_AD],
+        'coronary_AD': [input.coronary_AD],
+        'stroke': [input.stroke],
+        'CHF': [input.CHF],
+        'hypertension': [input.hypertension],
+        'COPD': [input.COPD],
+        'CKD': [input.CKD],
+        'malignancy': [input.malignancy],
+        'mental_illness': [input.mental_illness],
+        'creatinine': [input.creatinine],
+        'Hb_A1C': [input.Hb_A1C],
+        'albumin': [input.albumin],
+        'Hb_A1C_missing': [input.Hb_A1C_missing],
+        'creatinine_missing': [input.creatinine_missing],
+        'albumin_missing': [input.albumin_missing],
     }
     logger.debug(f"Prepared input data for model: {data}")
     df = pd.DataFrame(data)
@@ -169,18 +195,16 @@ def make_prediction(model, new_data: pd.DataFrame) -> dict:
         r_env = Environment()
         r_new_data = pandas2ri.py2rpy(new_data)
         r_env['r_new_data'] = r_new_data
-        r_env['follic_obj'] = model
+        r_env['model'] = model
 
         # Make predictions
         predict_func = robjects.r('predict')
-        predictions = predict_func(r_env['follic_obj'], newdata=r_env['r_new_data'])
+        predictions = predict_func(r_env['model'], newdata=r_env['r_new_data'])
         logger.debug("Prediction successful.")
         return predictions
     except Exception as e:
         logger.error("Error during R model prediction: %s", traceback.format_exc())
         raise e
-
-
 
 def extract_prediction_details(predictions, time_point: int) -> dict:
     cif = np.array(predictions.rx2('cif')[0])
@@ -215,16 +239,32 @@ def store_prediction(db: sqlite3.Connection, input: ModelInput, prediction_resul
     try:
         cursor = db.cursor()
         cursor.execute('''
-            INSERT INTO predictions (patientId, hospitalId, age, hgb, clinstg, ch, rt, predict_at, prediction)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            INSERT INTO predictions (
+                age, sex_f, elective_adm, homelessness, 
+                peripheral_AD, coronary_AD, stroke, CHF, hypertension, COPD, 
+                CKD, malignancy, mental_illness, creatinine, Hb_A1C, albumin, 
+                Hb_A1C_missing, creatinine_missing, albumin_missing, predict_at, prediction
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         ''', (
-            input.patientId,
-            input.hospitalId,
             input.age,
-            input.hgb,
-            input.clinstg,
-            input.ch,
-            input.rt,
+            input.sex_f,
+            input.elective_adm,
+            input.homelessness,
+            input.peripheral_AD,
+            input.coronary_AD,
+            input.stroke,
+            input.CHF,
+            input.hypertension,
+            input.COPD,
+            input.CKD,
+            input.malignancy,
+            input.mental_illness,
+            input.creatinine,
+            input.Hb_A1C,
+            input.albumin,
+            input.Hb_A1C_missing,
+            input.creatinine_missing,
+            input.albumin_missing,
             input.predict_at,
             json.dumps(prediction_result)
         ))
@@ -233,7 +273,6 @@ def store_prediction(db: sqlite3.Connection, input: ModelInput, prediction_resul
     except sqlite3.Error as e:
         logger.error("Database insertion error: %s", e)
         raise HTTPException(status_code=500, detail="Error inserting into the database")
-
 
 # FastAPI Endpoints
 @app.get("/", response_class=HTMLResponse)
@@ -244,12 +283,11 @@ def read_root(request: Request):
 async def favicon():
     return Response(status_code=204)
 
-
 @app.post("/predict")
 def predict(input: ModelInput, db: sqlite3.Connection = Depends(get_db)):
     try:
         new_data = prepare_input_data(input)
-        predictions = make_prediction(robjects.globalenv['follic.obj'], new_data)
+        predictions = make_prediction(robjects.globalenv['model'], new_data)
         prediction_result = extract_prediction_details(predictions, input.predict_at)
 
         store_prediction(db, input, prediction_result)
@@ -259,12 +297,17 @@ def predict(input: ModelInput, db: sqlite3.Connection = Depends(get_db)):
         logger.error("Error during prediction: %s", traceback.format_exc())
         raise HTTPException(status_code=500, detail="Error during prediction.")
 
-import json
 @app.get("/logs")
 def get_logs(db: sqlite3.Connection = Depends(get_db)):
+
+    query = '''
+            SELECT id, age, sex_f, elective_adm, homelessness, peripheral_AD, coronary_AD, stroke, CHF, hypertension, 
+            COPD, CKD, malignancy, mental_illness, creatinine, Hb_A1C, albumin, Hb_A1C_missing, creatinine_missing, 
+            albumin_missing, predict_at, prediction, timestamp FROM predictions ORDER BY timestamp DESC
+            '''
     try:
         cursor = db.cursor()
-        cursor.execute('SELECT id, patientId, hospitalId, age, hgb, clinstg, ch, rt, predict_at, prediction, timestamp FROM predictions ORDER BY timestamp DESC')
+        cursor.execute(query)
         rows = cursor.fetchall()
 
         logs = []
@@ -284,13 +327,25 @@ def get_logs(db: sqlite3.Connection = Depends(get_db)):
 
             log_entry = {
                 "id": row['id'],
-                "patientId": row['patientId'],
-                "hospitalId": row['hospitalId'],
                 "age": row['age'],
-                "hgb": row['hgb'],
-                "clinstg": row['clinstg'],
-                "ch": row['ch'],
-                "rt": row['rt'],
+                "sex_f": row['sex_f'],
+                "elective_adm": row['elective_adm'],
+                "homelessness": row['homelessness'],
+                "peripheral_AD": row['peripheral_AD'],
+                "coronary_AD": row['coronary_AD'],
+                "stroke": row['stroke'],
+                "CHF": row['CHF'],
+                "hypertension": row['hypertension'],
+                "COPD": row['COPD'],
+                "CKD": row['CKD'],
+                "malignancy": row['malignancy'],
+                "mental_illness": row['mental_illness'],
+                "creatinine": row['creatinine'],
+                "Hb_A1C": row['Hb_A1C'],
+                "albumin": row['albumin'],
+                "Hb_A1C_missing": row['Hb_A1C_missing'],
+                "creatinine_missing": row['creatinine_missing'],
+                "albumin_missing": row['albumin_missing'],
                 "predict_at": row['predict_at'],
                 "prediction": prediction_data,
                 "timestamp": row['timestamp']
@@ -299,11 +354,13 @@ def get_logs(db: sqlite3.Connection = Depends(get_db)):
 
         logger.info("Retrieved %d logs.", len(logs))
         return {"logs": logs}
-
     except sqlite3.Error as e:
         logger.error("Database retrieval error: %s", e)
         raise HTTPException(status_code=500, detail="Error retrieving logs from the database")
 
+    except sqlite3.Error as e:
+        logger.error("Database retrieval error: %s", e)
+        raise HTTPException(status_code=500, detail="Error retrieving logs from the database")
 
 @app.get("/health")
 def health_check():
